0ebf2803a8d1511254225fab1e4c8dc5f284a0d0
trunghoai123 <trunghoaiitiuh@gmail.com>
2023-09-15 08:27:34 +0700

    update

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
