4839896649f07a6746195a0865e372d94c2d7169
VSS\hoai.nguyen <trunghoaiitiuh@gmail.com>
2023-09-19 10:59:36 +0700

    dynamic router- render markdown

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
-import Head from "next/head";
-import Layout from "../components/layout";
-// export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// }
-export default function Home() {
+import Head from 'next/head';
+import Layout from '../components/layout';
+export async function getStaticProps() {
+  // getStaticProps - fetch data on any request
+  async function getServerSideProps(context) {
+    return {
+      props: {},
+    };
+  }
+  // static props - fetch data at build time
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title></title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
